80e42b617e2f399c036dabe81d3c8c7a71e7d63f
Ankur <anx450z@gmail.com>
2024-11-17 19:51:11 +0530

    Commit message: Add project_id to users and update specs
    - Add a migration to include project_id in the users table
    - Update schema to reflect the new project_id column
    - Ensure project_id is non-nullable and has a foreign key constraint
    - Modify project_spec.rb to streamline associations and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_16_093519) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_17_140629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,11 +54,14 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_16_093519) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "project_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["project_id"], name: "index_users_on_project_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "invites", "users"
   add_foreign_key "projects", "users"
   add_foreign_key "tokens", "users"
+  add_foreign_key "users", "projects"
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,26 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  describe 'validations' do
-    it { should validate_presence_of(:title) }
+  let(:user) { create(:user) }
+  let(:project) { build(:project, owner_id: user.id) }
+
+  describe 'Associations' do
+    it { is_expected.to have_many(:users) }
   end
 
-  describe 'associations' do
-    it { should have_many(:invites).dependent(:destroy) }
-    it { should have_many(:users).through(:invites) }
-  end
-
-  describe 'members management' do
-    let(:project) { create(:project_with_owner) }
-    let(:user) { create(:user) }
-
-    it 'has one owner' do
-      expect(project.invites.owner.count).to eq(1)
-    end
-
-    it 'can add members' do
-      invite = create(:invite, :accepted, project: project, user: user)
-      expect(project.users).to include(user)
-    end
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:title) }
   end
 end
