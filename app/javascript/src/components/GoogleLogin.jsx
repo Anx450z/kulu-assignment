cf25a81d47b63e6657af0ade96abeb3967088135
Ankur <anx450z@gmail.com>
2024-11-19 11:49:11 +0530

    Enhance GoogleLogin UI & Update Styles
    - Added project details and links in GoogleLogin component
    - Updated button text in GoogleLogin component
    - Changed button color and font weight in auth.css
    - Modified header style in auth.css
    - Set global background color in project.css
    - Adjusted project card border and shadow in project.css
    - Updated button background color in project.css

--- app/javascript/src/components/GoogleLogin.jsx
+++ app/javascript/src/components/GoogleLogin.jsx
@@ -9,10 +9,18 @@ const GoogleLogin = () => {
     <div className="login-container">
       <form method="post" action={`/users/auth/google_oauth2?type=signup`} >
       <input type="hidden" name="authenticity_token" value={csrfToken} />
+      <div>
+        <h1>Assignment: Project management</h1>
+        <br />
+        <h2>By: <a href="https://github.com/Anx450z">Ankur Singh Chauhan</a></h2>
+        <h3>Source Code: <a href="https://github.com/Anx450z/kulu-assignment">Github</a></h3>
+      </div>
+      <br />
+      <p>Begin by logging in</p>
       <button
           type="submit"
           className="google-login-button"
-        >Login with Google</button>
+        >Login with Google &gt;</button>
       </form>
     </div>
   )
